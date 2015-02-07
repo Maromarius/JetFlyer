207b4e84fe7633fccfba9c09502960ee1ace3bf8
Rami Kuret <ramikuret@gmail.com>
2015-02-07 15:47:01 -0500

    First part done

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate(){
+
+		bool jetpackActive = Input.GetButton("Fire1");
+
+		if(jetpackActive)
+		{
+			rigidbody2D.AddForce(new Vector2(0,jetpackForce));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ParticleSortingLayerFix.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortingLayerFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		particleSystem.renderer.sortingLayerName = "Player";
+		particleSystem.renderer.sortingOrder = -1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
