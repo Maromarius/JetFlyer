af08e1967d80932075bcf6c45dc4f6ca4c1f42b5
Rami Kuret <ramikuret@gmail.com>
2015-02-07 17:26:47 -0500

    adding a MainMenu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void LaunchGame()
+	{
+		Application.LoadLevel("RocketMouse");
+	}
+	public void Start(){
+
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+	}
+}
